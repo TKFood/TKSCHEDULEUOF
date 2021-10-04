ec2d18bd10ad65cee3a6722a7105713c7ee3a3a3
at3158 <tk290@tkfood.com.tw>
2021-10-04 14:36:15 +0800

    FrmParent

--- TKSCHEDULEUOF/FrmParent.cs
+++ TKSCHEDULEUOF/FrmParent.cs
@@ -11,6 +11,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TKITDLL;
 
 namespace TKSCHEDULEUOF
 {
@@ -38,9 +39,22 @@ namespace TKSCHEDULEUOF
             //Placing the control to the Form
             this.Controls.Add(MnuStrip);
 
+            //String connectionString;
+            //connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            //conn = new SqlConnection(connectionString);
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
             String connectionString;
-            connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
-            conn = new SqlConnection(connectionString);
+            conn = new SqlConnection(sqlsb.ConnectionString);
+
+
             String Sequel = "SELECT MAINMNU,MENUPARVAL,STATUS FROM MNU_PARENT";
             SqlDataAdapter da = new SqlDataAdapter(Sequel, conn);
             DataTable dt = new DataTable();
